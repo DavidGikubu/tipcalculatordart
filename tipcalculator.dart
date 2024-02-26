8b7c11dba3ab8e3465309eaf177ffe19bc2e3185
Gikubu <davidndungu8203@gmail.com>
2024-02-27 00:54:03 +0300

    a tip calculator app

---
+++ tipcalculator.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Welcome to the tip calculator!");
+  print("what was the total bill?");
+  int? totalBill = int.parse(stdin.readLineSync()!);
+  print("how much tip would you like to give? 10,12, or 15?");
+  int? tip = int.parse(stdin.readLineSync()!);
+  print("how many people to split the bill?");
+  int? totalNumberOfPeople = int.parse(stdin.readLineSync()!);
+}
